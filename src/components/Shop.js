56b0d0405443a5c99ba96dc1ee3f5a3e90228e96
coder2800 <devanshagarwal100@gmail.com>
2023-12-27 07:30:17 +0530

    Created a basic UI and setup redux functionality

---
+++ src/components/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <button className="btn btn-primary"> - </button>
+            Deposit/Withdraw Money
+        <button className="btn btn-primary"> + </button>
+    </div>
+  )
+}
+
+export default Shop
